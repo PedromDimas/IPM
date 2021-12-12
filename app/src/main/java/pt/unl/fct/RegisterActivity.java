dbbc4ab6cf87112483716dc149cc8d12f160dc96
JTokarz <just.tokarz@gmail.com>
2021-12-12 19:29:27 +0000

    let it be

--- app/src/main/java/pt/unl/fct/RegisterActivity.java
+++ app/src/main/java/pt/unl/fct/RegisterActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,6 +17,12 @@ public class RegisterActivity extends AppCompatActivity {
         TextView btn=findViewById(R.id.txtview_sign_in);
         btn.setOnClickListener(v -> {
             startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+        });
+
+        Button finish_registration_btn= findViewById(R.id.btn_register);
+        finish_registration_btn.setOnClickListener(v->{
+            Toast.makeText(this,"Registration succeeded!",Toast.LENGTH_LONG).show();
+            startActivity(new Intent(this,LoginActivity.class));
         });
     }
 }
